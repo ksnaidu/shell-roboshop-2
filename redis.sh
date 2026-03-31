eb1f4639a0228cbc9966b842096cfe47ff4e8c00
ksnaidu <ksnaidudba@gmail.com>
2026-03-31 12:26:10 +0530

    redis.sh added

---
+++ redis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=redis
+
+check_root
+
+dnf module disable redis -y &>>LOG_FILE
+VALIDATE $? "Disabling Redis version"
+
+dnf module enable redis:7 -y &>>LOG_FILE
+VALIDATE $? "Enabling redis version:7"
+
+dnf install redis -y &>>LOG_FILE
+VALIDATE $? "installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>LOG_FILE
+systemctl start redis 
+VALIDATE $? "Starting redis"
+
+print_time
+
